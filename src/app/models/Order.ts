1a2141f6f66a2f07ad961b4bd10a7621f908b180
Piroska Werner <piroska.werner@gmail.com>
2023-02-17 19:01:13 -0600

    Auto-generated commit [ci skip]

--- src/app/models/Order.ts
+++
@@ -1,9 +0,0 @@
-// import OrderItem model
-import { OrderItem } from './OrderItem';
-
-export class Order {
-  items: OrderItem[] = [];
-  status: 'active' | 'completed' = 'active';
-
-  constructor() {}
-}
